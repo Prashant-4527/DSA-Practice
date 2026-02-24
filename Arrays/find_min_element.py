454b2e4f3bc49b9c2b0ca239b17c05eae79ba91f
Prashant-4527 <Prashantsulaniya04@gmail.com>
2026-02-24 14:41:39 +0530

    find_min_element.py

---
+++ Arrays/find_min_element.py
@@ -0,0 +1,27 @@
+Problem: Find Minimum Element in Array
+
+# Approach:
+1. Assume first element is minimum
+2. traverse remaining elements 
+3. if current < min --- update
+4. return min
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+# Interview explaination:
+'''I maintain the smallest value seen so far while scanning the array once.
+each element is compared once, giving theta(n) time.
+Only constant auxiliary memeory is used, so space is O(1)'''
+
+Code:
+def find_min(arr):
+    if not arr:
+        return None
+    min_val = float('inf')
+
+    for x in arr:
+        if x < min_val:
+            min_val = x
+
+    return min_val
